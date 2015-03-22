4f482ba49a22ae818dea1a1a1d3995a26343e332
RomainGoncalves <goncalves.romain@gmail.com>
2015-03-22 01:57:50 +0100

    Directive for symptoms card

---
+++ www/js/directives/displaySymptom.js
@@ -0,0 +1,11 @@
+'use strict';
+
+angular.module('starter').directive('displaySymptom', [function() {
+    return {
+        restrict: 'EA',
+        templateUrl: 'templates/displaySymptoms.html',
+        link: function(scope) {
+            console.log(scope);
+        }
+    };
+}]);
