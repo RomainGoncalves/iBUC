b6e0b89bc7a73ee7ce3b05016dd43ab1e686ff9b
Matthieu Kern <matthieukern@gmail.com>
2015-03-22 12:19:13 +0100

    Fixed home controller

--- www/js/controllers/homeCtrl.js
+++ www/js/controllers/homeCtrl.js
@@ -5,23 +5,31 @@ angular.module('starter').controller('homeCtrl', ['$scope', '$firebaseObject', '
 	$firebaseObject(dataReferenceManagementService.getReference());
 
 	$scope.dataReference = [];
+	$scope.results = [];
 
 	dataReferenceManagementService.getReference().on('child_added', function(snapshot) {
-		$scope.dataReference.push(snapshot.val());
+		$scope.dataReference.unshift(snapshot);
+		$http.get('./json/references.json').then(function(references) {
+			$scope.results.unshift(analysisService.analyse(references.data, snapshot.val()));
+			console.log($scope.results);
+		});
 	});
 
 	dataReferenceManagementService.getReference().on('child_removed', function(snapshot) {
-		$scope.dataReference.splice($scope.dataReference.indexOf(snapshot), 1);
+		$scope.dataReference.every(function(elem, ind) {
+			console.log($scope.dataReference);
+			console.log(elem);
+			if (elem.key() == snapshot.key()) {
+				$scope.dataReference.splice(ind, 1);
+				$scope.results.splice(ind, 1);
+				return false;
+			}
+			return true;
+		});
 	});
 
 	$http.get('./json/cardHolder.json').then(function(cards){
 		$scope.cards = cards.data;
-	});
-
-	$http.get('./json/references.json').then(function(references) {
-		$http.get('./json/fakeData.json').then(function(data) {
-			$scope.results = analysisService.analyse(references.data, data.data[0]);
-		});
 	});
 
 	$scope.goTo = function(route){
